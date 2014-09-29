0a3949067e8cd3ef8422e2fe11e99e01c1bd29df
Mark Maglana <mmaglana@gmail.com>
2014-09-28 22:23:23 -0700

    Modify location of sample databases

--- shared/lesson03/reset_chat_db.sh
+++ shared/lesson03/reset_chat_db.sh
@@ -1,12 +1,12 @@
 ovs-appctl -t ovsdb-server exit
 
-if [ ! -d "/etc/chatdb" ]; then
-  mkdir /etc/chatdb
+if [ ! -d "/etc/lesson03" ]; then
+  mkdir /etc/lesson03
 else
-  rm /etc/chatdb/chat.db
+  rm /etc/lesson03/chat.db
 fi
 
-ovsdb-tool create /etc/chatdb/chat.db /vagrant/shared/lesson03/chat.ovsschema
-ovsdb-server --pidfile --detach --log-file --remote punix:/var/run/openvswitch/db.sock /etc/chatdb/chat.db
+ovsdb-tool create /etc/lesson03/chat.db /vagrant/shared/lesson03/chat.ovsschema
+ovsdb-server --pidfile --detach --log-file --remote punix:/var/run/openvswitch/db.sock /etc/lesson03/chat.db
 ovs-appctl -t ovsdb-server ovsdb-server/add-remote ptcp:6640
 ovs-appctl -t ovsdb-server vlog/set dbg
--- shared/lesson03/reset_todo_db.sh
+++ shared/lesson03/reset_todo_db.sh
@@ -1,5 +1,11 @@
 ovs-appctl -t ovsdb-server exit
-rm /etc/tododb/todo.db
-ovsdb-tool create /etc/tododb/todo.db /vagrant/shared/lesson03/todo.ovsschema
-ovsdb-server --pidfile --detach --log-file --remote punix:/var/run/openvswitch/db.sock /etc/tododb/todo.db
+
+if [ ! -d "/etc/lesson03" ]; then
+  mkdir /etc/lesson03
+else
+  rm /etc/lesson03/todo.db
+fi
+
+ovsdb-tool create /etc/lesson03/todo.db /vagrant/shared/lesson03/todo.ovsschema
+ovsdb-server --pidfile --detach --log-file --remote punix:/var/run/openvswitch/db.sock /etc/lesson03/todo.db
 ovs-appctl -t ovsdb-server ovsdb-server/add-remote ptcp:6640
